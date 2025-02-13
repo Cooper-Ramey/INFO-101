d4a44929705cea538706b8b8a3b11f9292ec252d
Cooper-Ramey <cramey@ucsb.edu>
2025-02-13 15:06:39 -0800

    Update exercises101c.R
    Part #2 is being wonky

--- exercises/exercises101c.R
+++ exercises/exercises101c.R
@@ -3,11 +3,15 @@ rovs <- data.frame(
   model = c("Chasing M2 Pro", "Qysea Fifish Pro V6", "BlueROV2"),
   price_usd = c(3459, 2999, 6500)
 )
-rovs[???, ???]
+rovs[1, 2]
+#Row 1, Col 2
 
 
 # Pull out the model column from rovs using both square bracket and dollar sign
 # indexing
+View(rovs)
+rovs[rovs$model, ]     # Not sure why this doesn't work...
+rovs$model     #This returns the first column
 
 
 # The file flowcytodata.csv contains artificial flow cytometry data, a technique
@@ -18,12 +22,10 @@ flowcyto <- read.csv("flowcytodata.csv")
 
 # Move the comments below so they go with the correct line of code.
 
-# Pull out the abundances from the first cast at the first station
 
-# Pull out all rows where abundance was more than 400
+flowcyto[flowcyto$abundance > 400, ]# Pull out all rows where abundance was more than 400
+flowcyto[flowcyto$station_id == 1 & flowcyto$cast == 1, 5]# Pull out the abundances from the first cast at the first station
+flowcyto[flowcyto$station_id == 1 & flowcyto$cast == 1, 5] <- NA# Set all the values in abundance to NA for the first cast at the first station
 
-# Set all the values in abundance to NA for the first cast at the first station
 
-flowcyto[flowcyto$abundance > 400, ]
-flowcyto[flowcyto$station_id == 1 & flowcyto$cast == 1, 5]
-flowcyto[flowcyto$station_id == 1 & flowcyto$cast == 1, 5] <- NA
+
